0af32d7bf70fb43973429baca3c6fcd061f0ec27
omerschles <omersch@campus.technion.ac.il>
2025-04-03 23:43:19 +0300

    filenames align between GT and images (perfect - per, imperfect - imp)

--- dataset.py
+++ dataset.py
@@ -1,133 +1,106 @@
 import os
 import glob
+from PIL import Image
 import torch
 from torch.utils.data import Dataset
-from PIL import Image
 import torchvision.transforms as T
 
-class MiddleburyFlowDataset(Dataset):
+class MovingObjectDisparityDataset(Dataset):
     """
-    Reads all PNG files in a single folder where files are named like:
-      {Scene}_{Version}_im0.png
-      {Scene}_{Version}_im1.png
-
-    For each matching 'im0'/'im1' pair, generates 4 sub-patches:
-      top-left, top-right, bottom-left, bottom-right
-    so that the dataset length = (#pairs * 4).
+    Custom Dataset for loading pairs of images and a chosen disparity map.
+    The folder structure:
+        dataset/
+          ├─ images/
+          │   ├─ object_state_im0.png (or .jpg, etc.)
+          │   ├─ object_state_im1.png
+          └─ GT/
+              ├─ object_state_disp0.png (or .jpg, etc.)
+              ├─ object_state_disp0y.png
+              ...
+    Example filenames:
+      - Backpack_perfect_im0.png
+      - Backpack_perfect_im1.png
+      - Backpack_per_disp0.jpg
+      - Backpack_per_disp0y.jpg
+      - etc.
     """
 
-    def __init__(self, data_dir, transform=None):
+    def __init__(self,
+                 root_dir,
+                 transform=None,
+                 use_disp_name='disp0'):
+        """
+        Args:
+            root_dir (str): Path to the dataset folder (containing 'images' and 'GT').
+            transform (callable, optional): Optional transform to be applied on a sample.
+            use_disp_name (str): Which disparity map pattern to use (e.g. 'disp0', 'disp0y', 'disp0-cd').
+        """
         super().__init__()
-        self.data_dir = data_dir
-        self.transform = transform  # To Tensor
+        self.root_dir = root_dir
+        self.images_dir = os.path.join(root_dir, 'images')
+        self.gt_dir = os.path.join(root_dir, 'GT')
+        self.transform = transform
+        self.use_disp_name = use_disp_name  # which GT map we want
 
-        # Gather all .png files
-        all_pngs = sorted(glob.glob(os.path.join(data_dir, '*.png')))
+        # Gather all im0 files
+        # For instance, anything ending in '_im0.*'
+        self.im0_list = sorted(glob.glob(os.path.join(self.images_dir, '*_im0.*')))
 
-        # Build a dict with key = (scene_version), storing paths for "im0" and "im1"
-        pairs_dict = {}
-        for path in all_pngs:
-            filename = os.path.basename(path)
-            name_no_ext = os.path.splitext(filename)[0]
-            parts = name_no_ext.split('_')
-            scene_version = '_'.join(parts[:-1])
-            im_label = parts[-1]
+        # We will create a list of tuples (im0_path, im1_path, disp_path)
+        self.samples = []
+        for im0_path in self.im0_list:
+            # Example:  Backpack_perfect_im0.png
+            # we want to also find Backpack_perfect_im1.png
+            file_name = os.path.basename(im0_path)
+            # remove '_im0' from name
+            common_prefix = file_name.rsplit('_im0', 1)[0]  # e.g. "Backpack_perfect"
+            ext = file_name.rsplit('.', 1)[1]               # e.g. "png"
 
-            if scene_version not in pairs_dict:
-                pairs_dict[scene_version] = {}
-            pairs_dict[scene_version][im_label] = path
+            # Build the corresponding im1 filename
+            im1_name = f"{common_prefix}_im1.{ext}"
+            im1_path = os.path.join(self.images_dir, im1_name)
+            if not os.path.exists(im1_path):
+                continue  # skip if we don't have a matching im1
 
-        self.pairs = []
-        for scene_version, paths in pairs_dict.items():
-            if 'im0' in paths and 'im1' in paths:
-                self.pairs.append((paths['im0'], paths['im1']))
+            # Build the disparity filename in GT folder
+            # For example, "Backpack_perfect_disp0.(extension?)"
+            # The extension in GT might differ, so we can attempt a small search:
+            disp_pattern = os.path.join(self.gt_dir, f"{common_prefix}_{self.use_disp_name}*")
+            found_disp = glob.glob(disp_pattern)
+            if not found_disp:
+                continue  # skip if no matching GT found
+            # assume the first match
+            disp_path = found_disp[0]
 
-        # Each pair produces 4 sub-patches => total samples = len(self.pairs) * 4
-        self.num_subpatches = 4
-        self.total_samples = len(self.pairs) * self.num_subpatches
+            self.samples.append((im0_path, im1_path, disp_path))
 
     def __len__(self):
-        return self.total_samples
+        return len(self.samples)
 
-    def __getitem__(self, index):
-        """
-        Map the index to (pair_index, subpatch_index).
-        pair_index = index // 4
-        subpatch_id = index % 4
-        """
-        pair_index = index // self.num_subpatches
-        subpatch_id = index % self.num_subpatches
+    def __getitem__(self, idx):
+        im0_path, im1_path, disp_path = self.samples[idx]
 
-        im0_path, im1_path = self.pairs[pair_index]
-        img0 = Image.open(im0_path).convert('RGB')
-        img1 = Image.open(im1_path).convert('RGB')
+        im0 = Image.open(im0_path).convert('RGB')
+        im1 = Image.open(im1_path).convert('RGB')
+        disp = Image.open(disp_path)
 
-        # Both images should have the same size
-        w, h = img0.size
+        # Convert to tensors
+        # Depending on your task, you might want to keep disparity as float
+        # and images scaled between [0,1].
+        to_tensor = T.ToTensor()
 
-        # Midpoints for splitting
-        mid_w = w // 2
-        mid_h = h // 2
+        im0_t = to_tensor(im0)
+        im1_t = to_tensor(im1)
+        disp_t = to_tensor(disp)  # e.g. grayscale => shape [1, H, W]
 
-        # subpatch_id: 0 => top-left, 1 => top-right, 2 => bottom-left, 3 => bottom-right
-        if subpatch_id == 0:
-            box = (0, 0, mid_w, mid_h)
-        elif subpatch_id == 1:
-            box = (mid_w, 0, w, mid_h)
-        elif subpatch_id == 2:
-            box = (0, mid_h, mid_w, h)
-        else:  # subpatch_id == 3
-            box = (mid_w, mid_h, w, h)
-
-        # Crop both images
-        img0_crop = img0.crop(box)
-        img1_crop = img1.crop(box)
+        # If you have some custom data augmentation, you can apply it here:
+        if self.transform is not None:
+            im0_t, im1_t, disp_t = self.transform(im0_t, im1_t, disp_t)
 
         sample = {
-            'img0': img0_crop,
-            'img1': img1_crop
+            'im0': im0_t,        # shape [3, H, W]
+            'im1': im1_t,        # shape [3, H, W]
+            'disp': disp_t       # shape [1, H, W] or [H, W] if not converted to 3D
         }
-
-        # Apply transform to get Tensors (and possibly augmentations)
-        if self.transform is not None:
-            sample = self.transform(sample)
 
         return sample
-
-
-# transform that simply converts the two cropped PIL images to tensors
-class FlowPatchTransform(object):
-    def __init__(self):
-        self.to_tensor = T.ToTensor()
-
-    def __call__(self, sample):
-        img0_pil = sample['img0']
-        img1_pil = sample['img1']
-
-        img0_tensor = self.to_tensor(img0_pil)
-        img1_tensor = self.to_tensor(img1_pil)
-
-        return {
-            'img0': img0_tensor,
-            'img1': img1_tensor
-        }
-
-
-if __name__ == "__main__":
-    data_dir = "dataset"
-
-    # Create the dataset with a transform that converts PIL images to Tensors
-    dataset = MiddleburyFlowDataset(data_dir=data_dir, transform=FlowPatchTransform())
-
-    print("Total samples:", len(dataset))  # #pairs * 4
-    sample0 = dataset[0]
-    print("Shape of img0:", sample0['img0'].shape)
-    print("Shape of img1:", sample0['img1'].shape)
-
-    # If you want to feed this into a model:
-    # from torch.utils.data import DataLoader
-    # loader = DataLoader(dataset, batch_size=4, shuffle=True)
-    # for batch in loader:
-    #     img0_batch = batch['img0']  # (B, 3, H/2, W/2)
-    #     img1_batch = batch['img1']  # (B, 3, H/2, W/2)
-    #     # pass to FlowNet, etc.
